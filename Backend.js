f38a070e67f203db13cfb2c01e54b560bb2cb204
Samuel <samuelvs186@gmail.com>
2026-04-24 16:36:21 -0300

    Primeiro commit

---
+++ Backend.js
@@ -0,0 +1,16 @@
+ const searchInput = document.getElementById("searchInput");
+  const cards = document.querySelectorAll(".card");
+
+  searchInput.addEventListener("input", () => {
+    const value = searchInput.value.toLowerCase();
+
+    cards.forEach(card => {
+      const text = card.dataset.name.toLowerCase();
+
+      if (text.includes(value)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
